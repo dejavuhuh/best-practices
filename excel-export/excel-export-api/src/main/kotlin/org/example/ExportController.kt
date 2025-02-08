191e5e94c14504a9362a863232ba1d3a294dd643
wuyue7570 <wuyue@musetech.group>
2025-02-08 17:24:20 +0800

    feat: redis pub/sub

--- excel-export/excel-export-api/src/main/kotlin/org/example/ExportController.kt
+++ excel-export/excel-export-api/src/main/kotlin/org/example/ExportController.kt
@@ -1,22 +1,21 @@
 package org.example
 
 import org.babyfish.jimmer.sql.kt.KSqlClient
-import org.springframework.beans.factory.DisposableBean
+import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.jdbc.core.ColumnMapRowMapper
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.queryForObject
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import kotlin.streams.asSequence
 
 @RestController
 @RequestMapping("/export")
 class ExportController(
     val sqlClient: KSqlClient,
-    val jdbcTemplate: JdbcTemplate
-) : DisposableBean {
-
-    val subscribers = mutableMapOf<Long, SseEmitter>()
+    val jdbcTemplate: JdbcTemplate,
+    val redisTemplate: StringRedisTemplate,
+    val exportEventChannel: ExportEventChannel
+) {
 
     data class AsyncExportRequest(
         val downloadFileName: String
@@ -28,15 +27,9 @@ class ExportController(
             downloadFileName = request.downloadFileName
         })
         val taskId = insertResult.modifiedEntity.id
-        val emitter = SseEmitter()
-        subscribers[taskId] = emitter
 
         Thread
             .ofVirtual()
-            .uncaughtExceptionHandler { _, e ->
-                e.printStackTrace()
-                emitter.completeWithError(e)
-            }
             .start {
                 val total = jdbcTemplate.queryForObject<Long>("SELECT count(*) FROM person")
                 var current = 0L
@@ -49,22 +42,13 @@ class ExportController(
                         Thread.sleep(20)
                         current += it.size
                         val progress = (current * 100 / total)
-                        emitter.send(SseEmitter.event().data(progress))
+                        redisTemplate.convertAndSend("export:$taskId", progress.toString())
                     }
-
-                emitter.send(SseEmitter.event().data(100))
-                emitter.complete()
             }
 
         return taskId
     }
 
     @GetMapping("/status")
-    fun status(@RequestParam taskId: Long): SseEmitter {
-        return subscribers[taskId] ?: throw IllegalArgumentException("Invalid task id")
-    }
-
-    override fun destroy() {
-        subscribers.values.forEach(SseEmitter::complete)
-    }
+    fun status(@RequestParam taskId: Long) = exportEventChannel.subscribe(taskId)
 }
---
+++ excel-export/excel-export-api/src/main/kotlin/org/example/ExportEventChannel.kt
@@ -0,0 +1,35 @@
+package org.example
+
+import org.springframework.data.redis.connection.Message
+import org.springframework.data.redis.connection.MessageListener
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+
+@Component
+class ExportEventChannel : MessageListener {
+
+    val subscribers = mutableMapOf<Long, MutableList<SseEmitter>>()
+
+    fun subscribe(taskId: Long): SseEmitter {
+        val emitter = SseEmitter()
+        emitter.onError {
+            subscribers[taskId]?.remove(emitter)
+        }
+        emitter.onCompletion {
+            subscribers[taskId]?.remove(emitter)
+        }
+
+        subscribers.computeIfAbsent(taskId) { mutableListOf() }.add(emitter)
+        return emitter
+    }
+
+    override fun onMessage(message: Message, pattern: ByteArray?) {
+        val topic = String(message.channel)
+        val taskId = topic.substringAfter("export:").toLong()
+        val progress = String(message.body)
+
+        subscribers[taskId]?.forEach {
+            it.send(progress)
+        }
+    }
+}
